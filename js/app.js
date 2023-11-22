92e6b8f84c4f7623a1d2cc9d0bc61c59d675fcdd
00015989wiut <saidovasilbek2023@gmail.com>
2023-11-22 22:04:42 +0500

    commit

--- js/app.js
+++ js/app.js
@@ -20,44 +20,77 @@ const messageText = document.querySelector(".message");
 const submitBtn = document.querySelector(".btn_submit");
 const result = document.querySelector("#result");
 
-// Checking Whether input is filled or not
-
-submitBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  if (inputName.value === "") {
-    inputName.style.borderColor = "red";
-    document.querySelector(".name").textContent = "Please enter your name !";
-  }
-  if (inputSurname.value === "") {
-    inputSurname.style.borderColor = "red";
-    document.querySelector(".surname").textContent =
-      "Please enter your surname !";
-  }
-  if (inputEmail.value === "") {
-    inputEmail.style.borderColor = "red";
-    document.querySelector(".email").textContent = "Please enter your email !";
-  }
-  if (messageText.value === "") {
-    messageText.style.borderColor = "red";
-    document.querySelector(".text").textContent =
-      "Please write any message here !";
-  }
-});
-
-// Counting text length
+// Text Count length
 
 let limit = 50;
 result.textContent = `0/${limit}`;
 
-messageText.addEventListener("input", () => {
+messageText.addEventListener("input", (e) => {
+  e.preventDefault();
   let lengthText = messageText.value.length;
-  result.textContent = `${lengthText}/${50}`;
-  if (textLength >= 50) {
+  result.textContent = `${lengthText}/${limit}`;
+  if (lengthText >= 50) {
     messageText.style.borderColor = "red";
     messageText.style.color = "red";
   } else {
     messageText.style.borderColor = "";
-    messageText.style.Color = "";
+    messageText.style.color = "";
+  }
+});
+
+// Checking Whether input is filled or not
+
+submitBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let allInputsValid = true;
+  if (inputName.value === "") {
+    inputName.style.borderColor = "red";
+    document.querySelector(".name").textContent = "Please enter your name!";
+    allInputsValid = false;
+  } else {
+    inputName.style.borderColor = "";
+    document.querySelector(".name").textContent = "";
+  }
+
+  if (inputSurname.value === "") {
+    inputSurname.style.borderColor = "red";
+    document.querySelector(".surname").textContent =
+      "Please enter your surname!";
+    allInputsValid = false;
+  } else {
+    inputSurname.style.borderColor = "";
+    document.querySelector(".surname").textContent = "";
+  }
+
+  if (inputEmail.value === "") {
+    inputEmail.style.borderColor = "red";
+    document.querySelector(".email").textContent = "Please enter your email!";
+    allInputsValid = false;
+  } else {
+    inputEmail.style.borderColor = "";
+    document.querySelector(".email").textContent = "";
+  }
+
+  if (messageText.value === "") {
+    messageText.style.borderColor = "red";
+    document.querySelector(".text").textContent =
+      "Please write any message here!";
+    allInputsValid = false;
+  } else {
+    messageText.style.borderColor = "";
+    document.querySelector(".text").textContent = "";
+  }
+
+  // Clearing Input values
+
+  if (allInputsValid) {
+    inputName.value = "";
+    inputEmail.value = "";
+    inputSurname.value = "";
+    messageText.value = "";
+
+    setTimeout(() => {
+      alert("Successfully Submitted Thanks!");
+    }, 1000);
   }
 });
